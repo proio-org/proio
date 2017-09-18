87fb0972f5174be9d55da7c18fd70f7102899c42
David Blyth <dblyth@asu.edu>
2017-09-18 15:45:02 -0500

    py-eicio: Working read-only, no-reference implementation
    Package can be install with `pip install .` in the py-eicio directory, or
    from pypi with `pip install eicio`.

---
+++ py-eicio/eicio/reader.py
@@ -0,0 +1,138 @@
+import gzip
+import struct
+
+from .event import Event
+import eicio.model as model
+from .writer import magicBytes
+
+class Reader:
+    """Reader for eicio files, either gzip compressed or not"""
+
+    def __init__(self, filename = "", fileobj = None, compress = False):
+        if fileobj != None:
+            if compress:
+                self.fileobj = gzip.GzipFile(fileobj = fileobj, mode = "rb")
+            else:
+                self.fileobj = fileobj
+        else:
+            if filename.endswith(".gz"):
+                self.fileobj = gzip.open(filename, "rb")
+            else:
+                self.fileobj = open(filename, "rb")
+
+    def close(self):
+        try:
+            self.fileobj.fileobj.close()
+        except AttributeError:
+            pass
+        self.fileobj.close()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.close()
+
+    def get(self):
+        n = self._syncToMagic()
+        if n < 4:
+            return
+        
+        sizes = struct.unpack("II", self.fileobj.read(8))
+        headerSize = sizes[0]
+        payloadSize = sizes[1]
+
+        headerString = self.fileobj.read(headerSize)
+        if len(headerString) != headerSize:
+            return
+        header = model.EventHeader.FromString(headerString)
+
+        payload = self.fileobj.read(payloadSize)
+        if len(payload) != payloadSize:
+            return
+
+        event = Event()
+        event.header = header
+        event._payload = payload
+
+        return event
+
+    def getHeader(self):
+        n = self._syncToMagic()
+        if n < 4:
+            return
+        
+        sizes = struct.unpack("II", self.fileobj.read(8))
+        headerSize = sizes[0]
+        payloadSize = sizes[1]
+
+        headerString = self.fileobj.read(headerSize)
+        if len(headerString) != headerSize:
+            return
+        header = model.EventHeader.FromString(headerString)
+
+        payload = self.fileobj.read(payloadSize)
+        if len(payload) != payloadSize:
+            return
+
+        return header
+
+    def _syncToMagic(self):
+        nRead = 0
+        while True:
+            magicByte = self.fileobj.read(1)
+            if len(magicByte) != 1:
+                return -1
+            nRead += 1
+
+            if magicByte == magicBytes[0]:
+                goodSeq = True
+                for i in range(1, 4):
+                    magicByte = self.fileobj.read(1)
+                    if len(magicByte) != 1:
+                        return -1
+                    nRead += 1
+
+                    if magicByte != magicBytes[i]:
+                        goodSeq = False
+                        break
+                if goodSeq:
+                    break
+
+        return nRead
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        event = self.get()
+        if event == None:
+            raise StopIteration
+        return event
+
+    def seekToStart(self):
+        if self.fileobj.seekable():
+            self.fileobj.seek(0, 0)
+
+    def skip(self, nEvents):
+        nSkipped = 0
+        for i in range(0, nEvents):
+            n = self._syncToMagic()
+            if n < 4:
+                return
+            
+            sizes = struct.unpack("II", self.fileobj.read(8))
+            headerSize = sizes[0]
+            payloadSize = sizes[1]
+
+            headerString = self.fileobj.read(headerSize)
+            if len(headerString) != headerSize:
+                return nSkipped
+
+            payload = self.fileobj.read(payloadSize)
+            if len(payload) != payloadSize:
+                return nSkipped
+
+            nSkipped += 1
+
+        return nSkipped
---
+++ py-eicio/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='eicio',
+      version='0.1.1',
+      description='Library for reading and writing EICIO files and streams',
+      url='http://github.com/decibelcooper/eicio',
+      author='David Blyth',
+      author_email='dblyth@anl.gov',
+      license='None',
+      packages=['eicio','eicio.model'],
+      zip_safe=True)
