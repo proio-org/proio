88d79546846dbed9ba111463b556964662fe3eb7
David Blyth <dblyth@asu.edu>
2017-09-26 16:56:19 -0500

    java-eicio: Started to fill in Reader class

--- java-eicio/src/main/java/eicio/List.java
+++ java-eicio/src/main/java/eicio/List.java
@@ -1,10 +1,35 @@
 package eicio;
 
+import java.io.IOException;
+
 public class List
 {
     public static void main( String[] args )
     {
-		for (int i = 0; i < args.length; i++)
-			System.out.println(args[i]);
+		if (args.length != 1) {
+			System.out.println("Please provide one argument");
+			return;
+		}
+
+		Reader reader = null;
+		try {
+			reader = new Reader(args[0]);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		if (reader != null) {
+			try {
+				reader.get();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		try {
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
     }
 }
--- java-eicio/src/main/java/eicio/Reader.java
+++ java-eicio/src/main/java/eicio/Reader.java
@@ -1,8 +1,82 @@
 package eicio;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.IOException;
+import java.util.zip.GZIPInputStream;
+
+import com.google.protobuf.Message;
+
 public class Reader
 {
-	public Reader() {
-		;
+	InputStream stream = null;
+	InputStream subStream = null;
+
+	public Reader(String filename) throws IOException {
+		try {
+			stream = new FileInputStream(filename);
+		} catch (FileNotFoundException e) {
+			throw new IOException("Failure to open input file", e);
+		}
+
+		if (filename.endsWith(".gz")) {
+			subStream = stream;
+			try {
+				this.stream = new GZIPInputStream(subStream);
+			} catch (IOException e) {
+				throw new IOException("Failure to create gzip stream", e);
+			}
+		}
+	}
+
+	public Reader(InputStream stream, boolean compress) throws IOException {
+		this.stream = stream;
+
+		if (compress) {
+			subStream = stream;
+			try {
+				this.stream = new GZIPInputStream(subStream);
+			} catch (IOException e) {
+				throw new IOException("Failure to create gzip stream", e);
+			}
+		}
+	}
+
+	public Reader(InputStream stream) throws IOException {
+		this(stream, false);
+	}
+
+	public void close() throws IOException {
+		try {
+			stream.close();
+		} catch (IOException e) {
+			throw new IOException("Failure to close stream", e);
+		}
+
+		if (subStream != null) {
+			try {
+				subStream.close();
+			} catch (IOException e) {
+				throw new IOException("Failure to close substream", e);
+			}
+		}
+	}
+
+	public Message get() throws IOException {
+		int n = this.syncToMagic();
+		return null;
+	}
+
+	private int syncToMagic() throws IOException {
+		for (int i = 0; i < 4; i++) {
+			try {
+				System.out.println(stream.read());
+			} catch (IOException e) {
+				throw new IOException("Failure to read magic number", e);
+			}
+		}
+
+		return 0;
 	}
 }
