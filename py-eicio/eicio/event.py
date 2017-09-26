079e623011b8aedf68d66b21b1a816701e8a8699
David Blyth <dblyth@asu.edu>
2017-09-26 12:09:38 -0500

    py-eicio: Changed API to conform to python naming conventions

--- py-eicio/eicio/event.py
+++ py-eicio/eicio/event.py
@@ -3,43 +3,43 @@ import eicio.model as model
 class Event:
     """Class representing a single event"""
 
-    def get(self, collName):
+    def get(self, coll_name):
         try:
-            return self._collCache[collName]
+            return self._coll_cache[coll_name]
         except AttributeError:
-            self._collCache = {}
+            self._coll_cache = {}
         except KeyError:
             pass
 
-        return self._getFromPayload(collName)
+        return self._get_from_payload(coll_name)
 
-    def getNames(self):
+    def get_names(self):
         names = []
-        for collHdr in self.header.payloadCollections:
-            names.append(collHdr.name)
+        for coll_hdr in self.header.payloadCollections:
+            names.append(coll_hdr.name)
 
         return names
 
-    def _getFromPayload(self, collName, unmarshal = True):
+    def _get_from_payload(self, coll_name, unmarshal = True):
         offset = 0
         size = 0
-        collType = ""
+        coll_type = ""
         for collIndex in range(0, len(self.header.payloadCollections)):
-            collHdr = self.header.payloadCollections[collIndex]
-            if collHdr.name == collName:
-                collType = collHdr.type
-                size = collHdr.payloadSize
+            coll_hdr = self.header.payloadCollections[collIndex]
+            if coll_hdr.name == coll_name:
+                coll_type = coll_hdr.type
+                size = coll_hdr.payloadSize
                 break
-            offset += collHdr.payloadSize
-        if collType == "":
+            offset += coll_hdr.payloadSize
+        if coll_type == "":
             return
 
         if unmarshal:
-            messageClass = getattr(model, collType)
-            message = messageClass.FromString(self._payload[offset : offset+size])
-            self._collCache[collName] = message
+            message_class = getattr(model, coll_type)
+            message = message_class.FromString(self._payload[offset : offset+size])
+            self._coll_cache[coll_name] = message
 
-        self.header.payloadCollections.remove(collHdr)
+        self.header.payloadCollections.remove(coll_hdr)
         self._payload = self._payload[:offset] + self._payload[offset+size:]
 
         return message
@@ -47,7 +47,7 @@ class Event:
     def dereference(self, ref):
         refColl = None
         try:
-            for name, coll in self._collCache.items():
+            for name, coll in self._coll_cache.items():
                 if coll.id == ref.collID:
                     if ref.entryID == 0:
                         return coll
@@ -57,9 +57,9 @@ class Event:
             pass
         
         if refColl == None:
-            for collHdr in self.header.payloadCollections:
-                if collHdr.id == ref.collID:
-                    refColl = self.get(collHdr.name)
+            for coll_hdr in self.header.payloadCollections:
+                if coll_hdr.id == ref.collID:
+                    refColl = self.get(coll_hdr.name)
                     if ref.entryID == 0:
                         return refColl
                     break
