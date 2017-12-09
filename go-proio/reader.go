637dc69be411181a8a18eee4f75764cb7838192c
David Blyth <dblyth@anl.gov>
2017-12-08 19:12:00 -0600

    go-proio/reader.go: fixed faulty logic in Skip()

--- go-proio/reader.go
+++ go-proio/reader.go
@@ -80,9 +80,10 @@ func (rdr *Reader) Skip(nEvents int) (nSkipped int, err error) {
 		bucketEventsLeft = int(rdr.bucketHeader.NEvents) - rdr.bucketEventsRead
 	}
 	if nEvents > bucketEventsLeft {
+        nSkipped += bucketEventsLeft
 		var n int
 		for n != 0 && err == nil {
-			n, err = rdr.readBucket(nEvents - bucketEventsLeft - nSkipped)
+			n, err = rdr.readBucket(nEvents - nSkipped)
 			if err != nil {
 				return
 			}
