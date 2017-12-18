6c2f8fdfdebb6426ebe47c15af88f3e07b783310
David Blyth <dblyth@anl.gov>
2017-12-18 16:21:43 -0600

    py-proio: added dummy model module for python2

--- py-proio/gen.sh
+++ py-proio/gen.sh
@@ -6,10 +6,12 @@ for proto in $(find ../model -iname "*.proto"); do
     protoc --proto_path=proio/model=../model --python_out=. $proto
 done
 
-for gen_file in $(find proio/model -iname "*_pb2.py"); do
-    mkdir ${gen_file%_pb2.py}
-    gen_dir=${gen_file%_pb2.py}
-    mv $gen_file $gen_dir/
-    gen_file=${gen_file##*/}
-    echo "from .${gen_file%.py} import *" > $gen_dir/__init__.py
+touch proio/model/__init__.py
+
+for gen_file in $(ls proio/model/*_pb2.py); do
+    mod_dir=${gen_file%_pb2.py}
+    mkdir $mod_dir &> /dev/null
+    mv $gen_file $mod_dir/
+    gen_mod=${gen_file##*/}
+    echo "from .${gen_mod%.py} import *" > $mod_dir/__init__.py
 done
