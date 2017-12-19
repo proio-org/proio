9688b0b6206b7c942c878ceb142d300b69937301
David Blyth <dblyth@anl.gov>
2017-12-19 16:05:23 -0600

    go-proio: rewrote benchmarks to be entirely in memory

--- go-proio/README.md
+++ go-proio/README.md
@@ -15,31 +15,7 @@ go get github.com/decibelcooper/proio/go-proio/...
 * [Skip](example_skip_test.go)
 * [Push, get, inspect](example_pushGetInspect_test.go)
 
-# Benchmarks
-With files:
-```
--rw-r--r-- 1 dblyth dblyth 2.8G Dec  4 19:14 repeatedSampleGZIP.proio
--rw-r--r-- 1 dblyth dblyth 3.8G Dec  4 19:01 repeatedSample.proio
--rw-r--r-- 1 dblyth dblyth 5.2G Dec  4 19:13 repeatedSampleUncomp.proio
--rw-r--r-- 1 dblyth dblyth 22M Oct  2 10:35 ../samples/largeSample.slcio
-```
-and after clearning the kernel page cache, I get
-```
-BenchmarkTracking-4       	   50000	    767342 ns/op
-BenchmarkTrackingLZ4-4    	   50000	    913342 ns/op
-BenchmarkTrackingGzip-4   	   50000	   1810212 ns/op
-BenchmarkTrackingLCIO-4   	     100	  22378964 ns/op
-```
-
-As a benchmark of the ability to scan the file headers, for
+# Benchmarking
 ```shell
-time proio-summary repeatedSample.proio
-```
-I get (after clearing the kernel page cache again)
-```
-Number of events: 50000
-
-real	0m0.035s
-user	0m0.009s
-sys	0m0.000s
+go test -bench=. -count 2
 ```
--- go-proio/bench_test.go
+++ go-proio/bench_test.go
@@ -1,243 +1,106 @@
 package proio // import "github.com/decibelcooper/proio/go-proio"
 
 import (
-	"math"
+	"bytes"
+	"math/rand"
 	"testing"
 
-	"go-hep.org/x/hep/lcio"
-
 	prolcio "github.com/decibelcooper/proio/go-proio/model/lcio"
+	//"go-hep.org/x/hep/lcio"
 )
 
-type TruthRelation struct {
-	Truth *prolcio.MCParticle
-	PNorm []float64
-	Eta   float64
-	P_T   float64
-}
-
-func normalizeVector(vector []float64) []float64 {
-	normFactor := math.Sqrt(dotProduct(vector, vector))
-	for i, value := range vector {
-		vector[i] = value / normFactor
+func doWrite(writer *Writer, b *testing.B) {
+	if b.N < 5000 {
+		b.N = 5000
 	}
-	return vector
-}
 
-func dotProduct(vector1 []float64, vector2 []float64) float64 {
-	return vector1[0]*vector2[0] + vector1[1]*vector2[1] + vector1[2]*vector2[2]
-}
+	event := NewEvent()
 
-func BenchmarkTracking(b *testing.B) {
-	filename := "repeatedSampleUncomp.proio"
-	reader, err := Open(filename)
-	if err != nil {
-		b.Skip("Skipping tracking benchmark: missing input file ", filename)
+	for i := 0; i < 1000; i++ {
+		event.AddEntry("SimCaloHits", &prolcio.SimCalorimeterHit{
+			Energy: rand.Float32(),
+			Pos: []float32{
+				rand.Float32(),
+				rand.Float32(),
+				rand.Float32(),
+			},
+		})
 	}
+
+	event.AddEntry("SimTrackHits", &prolcio.SimTrackerHit{
+		EDep: rand.Float32(),
+		Pos: []float64{
+			rand.Float64(),
+			rand.Float64(),
+			rand.Float64(),
+		},
+	})
 
 	b.ResetTimer()
-	tracking(reader, b)
-}
-
-func BenchmarkTrackingLZ4(b *testing.B) {
-	filename := "repeatedSample.proio"
-	reader, err := Open(filename)
-	if err != nil {
-		b.Skip("Skipping tracking benchmark: missing input file ", filename)
-	}
-
-	b.ResetTimer()
-	tracking(reader, b)
-}
-
-func BenchmarkTrackingGzip(b *testing.B) {
-	filename := "repeatedSampleGZIP.proio"
-	reader, err := Open(filename)
-	if err != nil {
-		b.Skip("Skipping tracking benchmark: missing input file ", filename)
-	}
-
-	b.ResetTimer()
-	tracking(reader, b)
-}
-
-type TruthRelationLCIO struct {
-	Truth *lcio.McParticle
-	PNorm [3]float64
-	Eta   float64
-	P_T   float64
-}
-
-func normalizeVectorLCIO(vector [3]float64) [3]float64 {
-	normFactor := math.Sqrt(dotProductLCIO(vector, vector))
-	for i, value := range vector {
-		vector[i] = value / normFactor
-	}
-	return vector
-}
-
-func dotProductLCIO(vector1 [3]float64, vector2 [3]float64) float64 {
-	return vector1[0]*vector2[0] + vector1[1]*vector2[1] + vector1[2]*vector2[2]
-}
-
-func BenchmarkTrackingLCIO(b *testing.B) {
-	filename := "../samples/largeSample.slcio"
-	reader, err := lcio.Open(filename)
-	if err != nil {
-		b.Skip("Skipping tracking benchmark: missing input file ", filename)
-	}
-
-	b.ResetTimer()
-	trackingLCIO(reader, b)
-}
-
-func tracking(reader *Reader, b *testing.B) {
-	b.N = 0
-	for event := range reader.ScanEvents() {
-		b.N++
-
-		truthParts := event.TaggedEntries("MCParticle")
-		tracks := event.TaggedEntries("Tracks")
-
-		// FIXME: boost back from crossing angle?
-
-		var truthRelations []TruthRelation
-		for _, truthID := range truthParts {
-			truth := event.GetEntry(truthID).(*prolcio.MCParticle)
-			if truth.GenStatus != 1 || truth.Charge == float32(0) {
-				continue
-			}
-
-			pNorm := normalizeVector(truth.P)
-			eta := math.Atanh(pNorm[2])
-			pT := math.Sqrt(truth.P[0]*truth.P[0] + truth.P[1]*truth.P[1])
-
-			if pT > 0.5 {
-				truthRelations = append(truthRelations, TruthRelation{
-					Truth: truth,
-					PNorm: pNorm,
-					Eta:   eta,
-					P_T:   pT,
-				})
-
-				/*
-					trueResults <- TrueResult{
-						Eta: eta,
-						P_T: pT,
-					}
-				*/
-			}
-		}
-
-		for _, trackID := range tracks {
-			track := event.GetEntry(trackID).(*prolcio.Track)
-			tanLambda := float64(track.States[0].TanL)
-
-			lambda := math.Atan(tanLambda)
-			px := math.Cos(float64(track.States[0].Phi)) * math.Cos(lambda)
-			py := math.Sin(float64(track.States[0].Phi)) * math.Cos(lambda)
-			pz := math.Sin(lambda)
-
-			pNorm := [3]float64{px, py, pz}
-
-			minAngle := math.Inf(1)
-			minIndex := -1
-			for i, truthRelation := range truthRelations {
-				angle := math.Acos(dotProduct(pNorm[:], truthRelation.PNorm))
-				if angle < minAngle {
-					minAngle = angle
-					minIndex = i
-				}
-			}
-
-			if minIndex >= 0 && minAngle < 0.01 {
-				/*
-					trackResults <- TrackResult{
-						MinAngle: minAngle,
-						Eta:      truthRelations[minIndex].Eta,
-						P_T:      truthRelations[minIndex].P_T,
-					}
-				*/
-
-				truthRelations = append(truthRelations[:minIndex], truthRelations[minIndex+1:]...)
-			}
-		}
-	}
-}
-
-func trackingLCIO(reader *lcio.Reader, b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		if !reader.Next() {
-			b.N = i
-			break
-		}
-
-		event := reader.Event()
-
-		truthColl := event.Get("MCParticle").(*lcio.McParticleContainer)
-		trackColl := event.Get("Tracks").(*lcio.TrackContainer)
-
-		// FIXME: boost back from crossing angle?
-
-		var truthRelations []TruthRelationLCIO
-		for i, truth := range truthColl.Particles {
-			if truth.GenStatus != 1 || truth.Charge == float32(0) {
-				continue
-			}
-
-			pNorm := normalizeVectorLCIO(truth.P)
-			eta := math.Atanh(pNorm[2])
-			pT := math.Sqrt(truth.P[0]*truth.P[0] + truth.P[1]*truth.P[1])
-
-			if pT > 0.5 {
-				truthRelations = append(truthRelations, TruthRelationLCIO{
-					Truth: &truthColl.Particles[i],
-					PNorm: pNorm,
-					Eta:   eta,
-					P_T:   pT,
-				})
-
-				/*
-					trueResults <- TrueResult{
-						Eta: eta,
-						P_T: pT,
-					}
-				*/
-			}
-		}
-
-		for _, track := range trackColl.Tracks {
-			tanLambda := track.TanL()
-			//eta := -math.Log(math.Sqrt(1+tanLambda*tanLambda) - tanLambda)
-
-			lambda := math.Atan(tanLambda)
-			px := math.Cos(track.Phi()) * math.Cos(lambda)
-			py := math.Sin(track.Phi()) * math.Cos(lambda)
-			pz := math.Sin(lambda)
-
-			pNorm := [3]float64{px, py, pz}
-
-			minAngle := math.Inf(1)
-			minIndex := -1
-			for i, truthRelation := range truthRelations {
-				angle := math.Acos(dotProductLCIO(pNorm, truthRelation.PNorm))
-				if angle < minAngle {
-					minAngle = angle
-					minIndex = i
-				}
-			}
-
-			if minIndex >= 0 && minAngle < 0.01 {
-				/*
-					trackResults <- TrackResult{
-						MinAngle: minAngle,
-						Eta:      truthRelations[minIndex].Eta,
-						P_T:      truthRelations[minIndex].P_T,
-					}
-				*/
-
-				truthRelations = append(truthRelations[:minIndex], truthRelations[minIndex+1:]...)
-			}
-		}
+		writer.Push(event)
 	}
+	writer.Flush()
+}
+
+func doRead(reader *Reader, b *testing.B) {
+	b.ResetTimer()
+	for event := range reader.ScanEvents() {
+		trackHitID := event.TaggedEntries("SimTrackHits")[0]
+		_ = event.GetEntry(trackHitID)
+	}
+}
+
+func BenchmarkWriteUncomp(b *testing.B) {
+	buffer := &bytes.Buffer{}
+	writer := NewWriter(buffer)
+	writer.SetCompression(UNCOMPRESSED)
+
+	doWrite(writer, b)
+}
+
+func BenchmarkWriteLZ4(b *testing.B) {
+	buffer := &bytes.Buffer{}
+	writer := NewWriter(buffer)
+	writer.SetCompression(LZ4)
+
+	doWrite(writer, b)
+}
+
+func BenchmarkWriteGZIP(b *testing.B) {
+	buffer := &bytes.Buffer{}
+	writer := NewWriter(buffer)
+	writer.SetCompression(GZIP)
+
+	doWrite(writer, b)
+}
+
+func BenchmarkReadUncomp(b *testing.B) {
+	buffer := &bytes.Buffer{}
+	writer := NewWriter(buffer)
+	writer.SetCompression(UNCOMPRESSED)
+	doWrite(writer, b)
+
+	reader := NewReader(buffer)
+	doRead(reader, b)
+}
+
+func BenchmarkReadLZ4(b *testing.B) {
+	buffer := &bytes.Buffer{}
+	writer := NewWriter(buffer)
+	writer.SetCompression(LZ4)
+	doWrite(writer, b)
+
+	reader := NewReader(buffer)
+	doRead(reader, b)
+}
+
+func BenchmarkReadGZIP(b *testing.B) {
+	buffer := &bytes.Buffer{}
+	writer := NewWriter(buffer)
+	writer.SetCompression(GZIP)
+	doWrite(writer, b)
+
+	reader := NewReader(buffer)
+	doRead(reader, b)
 }
