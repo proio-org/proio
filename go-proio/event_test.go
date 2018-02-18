9a1b36263bfa3d01c61751a8c47975239878c5bc
David Blyth <dblyth@anl.gov>
2018-02-18 16:29:33 -0600

    go-proio: consolodated non-example tests into 3 files

--- go-proio/event_test.go
+++ go-proio/event_test.go
@@ -7,6 +7,25 @@ import (
 	prolcio "github.com/decibelcooper/proio/go-proio/model/lcio"
 )
 
+func TestStrip1(t *testing.T) {
+	event := NewEvent()
+	for i := 0; i < 100; i++ {
+		event.AddEntry(
+			"Particle",
+			&eic.Particle{},
+		)
+	}
+
+	for _, ID := range event.TaggedEntries("Particle") {
+		event.RemoveEntry(ID)
+	}
+
+	nEntries := len(event.AllEntries())
+	if nEntries > 0 {
+		t.Errorf("There should be no entries, but len(event.AllEntries()) = %v out of 100", nEntries)
+	}
+}
+
 func TestTagUntag1(t *testing.T) {
 	event := NewEvent()
 	id0 := event.AddEntry(
