dce3b1e4acabd2957ee2ae7eeac93f08b104f9e2
David Blyth <dblyth@anl.gov>
2018-06-18 19:13:57 -0500

    java-proio: formatted with google-java-format

--- java-proio/src/main/java/proio/Event.java
+++ java-proio/src/main/java/proio/Event.java
@@ -1,103 +1,94 @@
 package proio;
 
-import java.lang.ClassNotFoundException;
-import java.lang.IllegalAccessException;
-import java.lang.NoSuchMethodException;
+import com.google.protobuf.ByteString;
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.protobuf.Message;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Vector;
-
-import com.google.protobuf.ByteString;
-import com.google.protobuf.InvalidProtocolBufferException;
-import com.google.protobuf.Message;
 import org.reflections.Reflections;
 
-public class Event
-{
-    protected Proto.Event eventProto = null;
-    protected Map<String, ByteString> metadata = new HashMap<String, ByteString>();
-    
-    private Map<Long, Message> entryCache = new HashMap<Long, Message>();
-    private Map<Long, Class> classCache = new HashMap<Long, Class>();
-    private static Reflections refl = new Reflections("proio.model");
+public class Event {
+  protected Proto.Event eventProto = null;
+  protected Map<String, ByteString> metadata = new HashMap<String, ByteString>();
 
-	public Event() {}
+  private Map<Long, Message> entryCache = new HashMap<Long, Message>();
+  private Map<Long, Class> classCache = new HashMap<Long, Class>();
+  private static Reflections refl = new Reflections("proio.model");
 
-    public Iterable<Long> getAllEntries() {
-        Vector<Long> entries = new Vector<Long>();
-        if (eventProto != null) {
-            for (Map.Entry<Long, Proto.Any> entry : eventProto.getEntryMap().entrySet()) {
-                entries.add(entry.getKey());
-            }
-        }
-        return entries;
+  public Event() {}
+
+  public Iterable<Long> getAllEntries() {
+    Vector<Long> entries = new Vector<Long>();
+    if (eventProto != null) {
+      for (Map.Entry<Long, Proto.Any> entry : eventProto.getEntryMap().entrySet()) {
+        entries.add(entry.getKey());
+      }
+    }
+    return entries;
+  }
+
+  public Iterable<Long> getTaggedEntries(String tag) {
+    if (eventProto == null || !eventProto.containsTag(tag)) {
+      return new Vector<Long>();
     }
 
-    public Iterable<Long> getTaggedEntries(String tag) {
-        if (eventProto == null || !eventProto.containsTag(tag)) {
-            return new Vector<Long>();
-        }
+    return eventProto.getTagMap().get(tag).getEntryList();
+  }
 
-        return eventProto.getTagMap().get(tag).getEntryList();
+  public Message getEntry(long id)
+      throws NoSuchMethodException, IllegalAccessException, InvalidProtocolBufferException,
+          ClassNotFoundException, InvocationTargetException {
+    if (entryCache.containsKey(id)) {
+      return entryCache.get(id);
     }
 
-    public Message getEntry(long id)
-            throws NoSuchMethodException,
-              IllegalAccessException,
-              InvalidProtocolBufferException,
-              ClassNotFoundException,
-              InvocationTargetException {
-        if (entryCache.containsKey(id)) {
-            return entryCache.get(id);
-        }
-
-        if (!eventProto.getEntryMap().containsKey(id)) {
-            return null;
-        }
-        Proto.Any entryProto = eventProto.getEntryMap().get(id);
-
-        Class entryClass = getClass(entryProto.getType());
-        Method newBuilder = entryClass.getMethod("newBuilder");
-        Message.Builder builder = (Message.Builder)newBuilder.invoke(null);
-        builder.mergeFrom(entryProto.getPayload());
-        Message entry = builder.build();
-
-        entryCache.put(id, entry);
-        return entry;
+    if (!eventProto.getEntryMap().containsKey(id)) {
+      return null;
     }
+    Proto.Any entryProto = eventProto.getEntryMap().get(id);
 
-    public Iterable<String> getTags() {
-        Vector<String> tags = new Vector<String>();
-        if (eventProto != null) {
-            for (Map.Entry<String, Proto.Tag> entry : eventProto.getTagMap().entrySet()) {
-                tags.add(entry.getKey());
-            }
-        }
-        Collections.sort(tags);
-        return tags;
+    Class entryClass = getClass(entryProto.getType());
+    Method newBuilder = entryClass.getMethod("newBuilder");
+    Message.Builder builder = (Message.Builder) newBuilder.invoke(null);
+    builder.mergeFrom(entryProto.getPayload());
+    Message entry = builder.build();
+
+    entryCache.put(id, entry);
+    return entry;
+  }
+
+  public Iterable<String> getTags() {
+    Vector<String> tags = new Vector<String>();
+    if (eventProto != null) {
+      for (Map.Entry<String, Proto.Tag> entry : eventProto.getTagMap().entrySet()) {
+        tags.add(entry.getKey());
+      }
     }
+    Collections.sort(tags);
+    return tags;
+  }
 
-    public void clear() {
-        eventProto = null;
-        metadata.clear();
-        return;
-    };
+  public void clear() {
+    eventProto = null;
+    metadata.clear();
+    return;
+  };
 
-    private Class getClass(long typeID)
-            throws ClassNotFoundException {
-        if (!classCache.containsKey(typeID)) {
-            String typeName = eventProto.getTypeMap().get(typeID).toLowerCase();
-            for (Class thisClass : refl.getSubTypesOf(Message.class)) {
-                String thisName = thisClass.getName().replace('$', '.').toLowerCase();
-                if (thisName.equals(typeName)) {
-                    classCache.put(typeID, thisClass);
-                    return thisClass;
-                }
-            }
+  private Class getClass(long typeID) throws ClassNotFoundException {
+    if (!classCache.containsKey(typeID)) {
+      String typeName = eventProto.getTypeMap().get(typeID).toLowerCase();
+      for (Class thisClass : refl.getSubTypesOf(Message.class)) {
+        String thisName = thisClass.getName().replace('$', '.').toLowerCase();
+        if (thisName.equals(typeName)) {
+          classCache.put(typeID, thisClass);
+          return thisClass;
         }
-        return classCache.get(typeID);
+      }
     }
+    return classCache.get(typeID);
+  }
 }
--- java-proio/src/main/java/proio/Ls.java
+++ java-proio/src/main/java/proio/Ls.java
@@ -1,101 +1,95 @@
 package proio;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
 import com.google.protobuf.Descriptors;
 import com.google.protobuf.Message;
+import java.util.List;
 
-public class Ls
-{
-    public static void main( String[] args )
-    {
-		if (args.length != 1) {
-			System.out.println("Please provide one argument for the input file name");
-			return;
-		}
-
-		try {
-			Reader reader = new Reader(args[0]);
-
-            int nEvents = 0;
-			for (Event event : reader) {
-                System.out.println("EVENT: " + nEvents);
-                for (String tag : event.getTags()) {
-                    System.out.println(tag);
-                    for (long id : event.getTaggedEntries(tag)) {
-                        Message entry = event.getEntry(id);
-                        System.out.println("-------------------");
-                        System.out.println(getMessageString(entry).replaceAll("\n", "\n\t"));
-                        System.out.println("-------------------");
-                    }
-                }
-                nEvents++;
-			}
-
-			reader.close();
-		} catch (Throwable e) {
-			e.printStackTrace();
-		}
+public class Ls {
+  public static void main(String[] args) {
+    if (args.length != 1) {
+      System.out.println("Please provide one argument for the input file name");
+      return;
     }
 
-	// Performs heavy lifting for collection entry introspection
-	private static String getMessageString(Message msg) {
-		String returnString = "";
+    try {
+      Reader reader = new Reader(args[0]);
 
-		Descriptors.Descriptor desc = msg.getDescriptorForType();
-		List<Descriptors.FieldDescriptor> fields = desc.getFields();
+      int nEvents = 0;
+      for (Event event : reader) {
+        System.out.println("EVENT: " + nEvents);
+        for (String tag : event.getTags()) {
+          System.out.println(tag);
+          for (long id : event.getTaggedEntries(tag)) {
+            Message entry = event.getEntry(id);
+            System.out.println("-------------------");
+            System.out.println(getMessageString(entry).replaceAll("\n", "\n\t"));
+            System.out.println("-------------------");
+          }
+        }
+        nEvents++;
+      }
 
-		for (Descriptors.FieldDescriptor field : fields) {
-			if (!field.isRepeated()) {
-				if (msg.hasField(field)) {
-					returnString = returnString + "\n" + field.getName() + ": ";
-					Object value = msg.getField(field);
-					returnString = returnString + getFieldValueString(field, value);
-				}
-			} else {
-				int count = msg.getRepeatedFieldCount(field);
-				for (int i = 0; i < count; i++) {
-					returnString = returnString + "\n" + field.getName() + "[" + Integer.toString(i) + "]: ";
-					Object value = msg.getRepeatedField(field, i);
-					String fieldString = getFieldValueString(field, value);
-					returnString = returnString + fieldString;
-				}
-			}
-		}
+      reader.close();
+    } catch (Throwable e) {
+      e.printStackTrace();
+    }
+  }
 
-		return returnString;
-	}
+  // Performs heavy lifting for collection entry introspection
+  private static String getMessageString(Message msg) {
+    String returnString = "";
 
-	// Performs heavy lifting for collection entry introspection
-	private static String getFieldValueString(Descriptors.FieldDescriptor field, Object value) {
-		String returnString = "";
+    Descriptors.Descriptor desc = msg.getDescriptorForType();
+    List<Descriptors.FieldDescriptor> fields = desc.getFields();
 
-		switch (field.getType()) {
-			case INT32:
-			case UINT32:
-				returnString = Integer.toString((Integer)value);
-				break;
-			case INT64:
-			case UINT64:
-				returnString = Long.toString((Long)value);
-				break;
-			case FLOAT:
-				returnString = Float.toString((Float)value);
-				break;
-			case DOUBLE:
-				returnString = Double.toString((Double)value);
-				break;
-			case STRING:
-				returnString = (String)value;
-				break;
-			case MESSAGE:
-				returnString = getMessageString((Message)value).replaceAll("\n", "\n\t");
-				break;
-		}
+    for (Descriptors.FieldDescriptor field : fields) {
+      if (!field.isRepeated()) {
+        if (msg.hasField(field)) {
+          returnString = returnString + "\n" + field.getName() + ": ";
+          Object value = msg.getField(field);
+          returnString = returnString + getFieldValueString(field, value);
+        }
+      } else {
+        int count = msg.getRepeatedFieldCount(field);
+        for (int i = 0; i < count; i++) {
+          returnString = returnString + "\n" + field.getName() + "[" + Integer.toString(i) + "]: ";
+          Object value = msg.getRepeatedField(field, i);
+          String fieldString = getFieldValueString(field, value);
+          returnString = returnString + fieldString;
+        }
+      }
+    }
 
-		return returnString;
-	}
+    return returnString;
+  }
+
+  // Performs heavy lifting for collection entry introspection
+  private static String getFieldValueString(Descriptors.FieldDescriptor field, Object value) {
+    String returnString = "";
+
+    switch (field.getType()) {
+      case INT32:
+      case UINT32:
+        returnString = Integer.toString((Integer) value);
+        break;
+      case INT64:
+      case UINT64:
+        returnString = Long.toString((Long) value);
+        break;
+      case FLOAT:
+        returnString = Float.toString((Float) value);
+        break;
+      case DOUBLE:
+        returnString = Double.toString((Double) value);
+        break;
+      case STRING:
+        returnString = (String) value;
+        break;
+      case MESSAGE:
+        returnString = getMessageString((Message) value).replaceAll("\n", "\n\t");
+        break;
+    }
+
+    return returnString;
+  }
 }
-
