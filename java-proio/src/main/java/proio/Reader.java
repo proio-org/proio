55393d4881be62502ca3915e3c7b5976786e5bdc
David Blyth <dblyth@anl.gov>
2018-07-12 09:36:16 -0500

    java-proio: Reader: fix for metadata only case

--- java-proio/src/main/java/proio/Reader.java
+++ java-proio/src/main/java/proio/Reader.java
@@ -56,6 +56,8 @@ public class Reader implements Iterable<Event>, Iterator<Event> {
         readBucket();
         readFromBucket(event);
       }
+    } else {
+        bucketIndex++;
     }
 
     return event;
