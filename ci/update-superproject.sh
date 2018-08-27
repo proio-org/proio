7d99af9178aed4bff018d41c574e452e33bc55f2
David Blyth <dblyth@anl.gov>
2018-08-27 14:06:26 -0500

    Fix for superproject update

--- ci/update-superproject.sh
+++ ci/update-superproject.sh
@@ -4,6 +4,6 @@ git clone http://git@$REPO_REF repo
 cd repo
 git submodule init $PROIO_SUBMODULE_PATH
 git submodule update --remote $PROIO_SUBMODULE_PATH
-git add proio
+git add $PROIO_SUBMODULE_PATH
 git commit -m "Automatic update of proio submodule from proio Travis CI" -m "proio repository commit: $TRAVIS_COMMIT"
 git push "https://$REPO_TOKEN@$REPO_REF"
