0d6c3949a444aa5b2c82e706cae35a02801e0190
David Blyth <dblyth@anl.gov>
2018-08-27 13:58:57 -0500

    Changed the way superproject submodule updates are done

--- ci/update-superproject.sh
+++ ci/update-superproject.sh
@@ -2,8 +2,8 @@
 
 git clone http://git@$REPO_REF repo
 cd repo
-git submodule init proio
-git submodule update --remote proio
+git submodule init $PROIO_SUBMODULE_PATH
+git submodule update --remote $PROIO_SUBMODULE_PATH
 git add proio
 git commit -m "Automatic update of proio submodule from proio Travis CI" -m "proio repository commit: $TRAVIS_COMMIT"
 git push "https://$REPO_TOKEN@$REPO_REF"
